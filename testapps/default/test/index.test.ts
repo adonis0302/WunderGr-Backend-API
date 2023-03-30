adonis0302/WunderGr-Backend-API

commit e5ffde8c34be861b67c0f0e940e4311053b3d17b

fix: support TS operations that never return (#762)
We didn't support operations that didn't return, which can be useful as e.g.
stubs during development (i.e. an operation that always throws an "unimplemented" error).
To support these, make ResponseSchema an optional and update callers using this field
to deal with it being possible undefined.

If an operation has no defined response schema, we now omit a 200 response from its
OpenAPI definition. Update tests to reflect these changes too.

packages/sdk/src/configure/index.ts
@@ -1328,10 +1328,12 @@ const typeScriptOperationsResponseSchemas = async (wgDirAbs: string, operations:
 		}
 	}
 	for (const op of operations) {
-		const schema = generator.getSchemaForSymbol(responseTypeName(op));
-		if (schema) {
+		try {
+			const schema = generator.getSchemaForSymbol(responseTypeName(op));
 			delete schema.$schema;
 			schemas[op.Name] = schema as JSONSchema;
+		} catch (e: any) {
+			Logger.warn(`could not generate response schema for ${op.Name}: ${e}`);
 		}
 	}
 	console.warn = originalWarn;
@@ -1344,9 +1346,16 @@ const typeScriptOperationsResponseSchemas = async (wgDirAbs: string, operations:
 const updateTypeScriptOperationsResponseSchemas = async (wgDirAbs: string, operations: GraphQLOperation[]) => {
 	const schemas = await typeScriptOperationsResponseSchemas(wgDirAbs, operations);
 	for (const op of operations) {
-		const responseSchema = schemas[op.Name];
-		if (responseSchema) {
-			op.ResponseSchema = responseSchema;
+		const schema = schemas[op.Name];
+		if (schema) {
+			op.ResponseSchema = schemas[schema];
+		} else {
+			// For functions that don't return anything, we return an empty JSON object
+			op.ResponseSchema = {
+				type: 'object',
+				additionalProperties: false,
+				properties: {},
+			};
 		}
 	}
 };

packages/sdk/src/openapibuilder/index.test.ts
@@ -2,54 +2,44 @@ import { OperationExecutionEngine, OperationType } from '@wundergraph/protobuf';
 import { GraphQLOperation } from '../graphql/operations';
 import { OpenApiBuilder } from './index';
 
+const emptySchema = {
+	type: 'object',
+	properties: {},
+	additionalProperties: false,
+};
+
 const operations = [
 	{
 		Name: 'GetRootObj',
 		PathName: 'GetRootObj',
 		OperationType: OperationType.QUERY,
 		ExecutionEngine: OperationExecutionEngine.ENGINE_GRAPHQL,
-		VariablesSchema: {
-			type: 'object',
-			properties: {},
-			additionalProperties: false,
-			definitions: {},
-		},
+		VariablesSchema: emptySchema,
+		ResponseSchema: emptySchema,
 	},
 	{
 		Name: 'GetRootObj2',
 		PathName: 'GetRootObj2',
 		OperationType: OperationType.QUERY,
 		ExecutionEngine: OperationExecutionEngine.ENGINE_GRAPHQL,
-		VariablesSchema: {
-			type: 'object',
-			properties: {},
-			additionalProperties: false,
-			definitions: {},
-		},
+		VariablesSchema: emptySchema,
+		ResponseSchema: emptySchema,
 	},
 	{
 		Name: 'GetUser',
 		PathName: 'users/get',
 		OperationType: OperationType.QUERY,
 		ExecutionEngine: OperationExecutionEngine.ENGINE_GRAPHQL,
-		VariablesSchema: {
-			type: 'object',
-			properties: {},
-			additionalProperties: false,
-			definitions: {},
-		},
+		VariablesSchema: emptySchema,
+		ResponseSchema: emptySchema,
 	},
 	{
 		Name: 'SubscribeToUser',
 		PathName: 'users/subscribe',
 		OperationType: OperationType.SUBSCRIPTION,
 		ExecutionEngine: OperationExecutionEngine.ENGINE_GRAPHQL,
-		VariablesSchema: {
-			type: 'object',
-			properties: {},
-			additionalProperties: false,
-			definitions: {},
-		},
+		VariablesSchema: emptySchema,
+		ResponseSchema: emptySchema,
 	},
 	{
 		Name: 'PutUser',
@@ -57,12 +47,8 @@ const operations = [
 		OperationType: OperationType.MUTATION,
 		ExecutionEngine: OperationExecutionEngine.ENGINE_GRAPHQL,
 		AuthenticationConfig: { required: true },
-		VariablesSchema: {
-			type: 'object',
-			properties: {},
-			additionalProperties: false,
-			definitions: {},
-		},
+		VariablesSchema: emptySchema,
+		ResponseSchema: emptySchema,
 	},
 ] as GraphQLOperation[];
 

packages/sdk/src/openapibuilder/__snapshots__/index.test.ts.snap
@@ -54,7 +54,11 @@ exports[`OpenAPI builder OpenAPI Builder 1`] = `
           "200": {
             "content": {
               "application/json": {
-                "schema": undefined,
+                "schema": {
+                  "additionalProperties": false,
+                  "properties": {},
+                  "type": "object",
+                },
               },
             },
             "description": "Success",
@@ -82,7 +86,11 @@ exports[`OpenAPI builder OpenAPI Builder 1`] = `
           "200": {
             "content": {
               "application/json": {
-                "schema": undefined,
+                "schema": {
+                  "additionalProperties": false,
+                  "properties": {},
+                  "type": "object",
+                },
               },
             },
             "description": "Success",
@@ -110,7 +118,11 @@ exports[`OpenAPI builder OpenAPI Builder 1`] = `
           "200": {
             "content": {
               "application/json": {
-                "schema": undefined,
+                "schema": {
+                  "additionalProperties": false,
+                  "properties": {},
+                  "type": "object",
+                },
               },
             },
             "description": "Success",
@@ -138,7 +150,6 @@ exports[`OpenAPI builder OpenAPI Builder 1`] = `
             "application/json": {
               "schema": {
                 "additionalProperties": false,
-                "definitions": {},
                 "properties": {},
                 "type": "object",
               },
@@ -150,7 +161,11 @@ exports[`OpenAPI builder OpenAPI Builder 1`] = `
           "200": {
             "content": {
               "application/json": {
-                "schema": undefined,
+                "schema": {
+                  "additionalProperties": false,
+                  "properties": {},
+                  "type": "object",
+                },
               },
             },
             "description": "Success",
@@ -178,7 +193,11 @@ exports[`OpenAPI builder OpenAPI Builder 1`] = `
           "200": {
             "content": {
               "application/json": {
-                "schema": undefined,
+                "schema": {
+                  "additionalProperties": false,
+                  "properties": {},
+                  "type": "object",
+                },
               },
             },
             "description": "Success",

commit a3f93b6ee3a3960254cd286fb7246845c387e3be

fix: make TS operation response schema generation more forgiving (#776)
* fix: make TS operation response schema more forgiving

- Load only the .ts file with the response types instead of the whole
program
- Ignore non-fatal errors during schema generation
- Correctly set the schema in updateTypeScriptOperationsResponseSchemas()
before passing it to the OpenAPI builder (broken in #762)
- Add test for OpenAPI spec generation

packages/sdk/src/configure/index.ts
@@ -1284,7 +1284,7 @@ const typeScriptOperationsResponseSchemas = async (wgDirAbs: string, operations:
 	const cacheKey = `ts.operationTypes.${objectHash([contents, operationHashes])}`;
 	const cachedData = await cache.getJSON(cacheKey);
 	if (cachedData) {
-		return cachedData;
+		return cachedData as Record<string, JSONSchema>;
 	}
 
 	const basePath = path.join(wgDirAbs, 'generated');
@@ -1294,6 +1294,7 @@ const typeScriptOperationsResponseSchemas = async (wgDirAbs: string, operations:
 
 	const settings = {
 		required: true,
+		ignoreErrors: true,
 	};
 
 	// XXX: There's no way to silence warnings from TJS, override console.warn
@@ -1303,7 +1304,7 @@ const typeScriptOperationsResponseSchemas = async (wgDirAbs: string, operations:
 	const tsConfigPath = await findUp('tsconfig.json', wgDirAbs);
 	let generator: JsonSchemaGenerator | null = null;
 	if (tsConfigPath) {
-		const tsConfigProgram = programFromConfig(tsConfigPath);
+		const tsConfigProgram = programFromConfig(tsConfigPath, [programPath]);
 		if (tsConfigProgram) {
 			generator = buildGenerator(tsConfigProgram, settings);
 		}
@@ -1343,7 +1344,7 @@ const updateTypeScriptOperationsResponseSchemas = async (wgDirAbs: string, opera
 	for (const op of operations) {
 		const schema = schemas[op.Name];
 		if (schema) {
-			op.ResponseSchema = schemas[schema];
+			op.ResponseSchema = schema;
 		} else {
 			// For functions that don't return anything, we return an empty JSON object
 			op.ResponseSchema = {

testapps/default/test/index.test.ts
@@ -1,6 +1,10 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
 import { InputValidationError } from '@wundergraph/sdk/client';
 import { createTestServer } from '../.wundergraph/generated/testing';
+import users from '../.wundergraph/operations/nested/users';
 
 const wg = createTestServer();
 
@@ -175,5 +179,20 @@ describe('@transform directive', () => {
 		expect(country).toBeDefined();
 		expect(country?.capital).toBe('Madrid');
 		expect(country?.weather.temperature?.max).toBeDefined();
+	});
+});
+
+describe('OpenAPI generation', () => {
+	test('OpenAPI includes TypeScript operation response schema', async () => {
+		const filePath = path.join(__dirname, '..', '.wundergraph', 'generated', 'wundergraph.openapi.json');
+		const data = await fs.readFile(filePath, { encoding: 'utf-8' });
+		const spec = JSON.parse(data);
+		const usersGet = spec.paths?.['/users/get'];
+		expect(usersGet).toBeDefined();
+		const response = usersGet?.['get']?.['responses']?.['200'];
+		expect(response).toBeDefined();
+		const responseSchema = response['content']?.['application/json']?.['schema'];
+		expect(responseSchema).toBeDefined();
+		expect(responseSchema['properties']?.['hello']?.['type']).toBe('string');
 	});
 });